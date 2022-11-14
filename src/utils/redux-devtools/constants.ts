00c930872677cfe73dea0de6574148c83c79d1fc
Paul <pavlobu@gmail.com>
2022-11-14 22:35:00 +0100

    better comments in guide

--- src/utils/redux-devtools/constants.ts
+++ src/utils/redux-devtools/constants.ts
@@ -3,7 +3,10 @@ export const actionKey = 'lastAction';
 
 /**
  * NOTE: the variable below should be used across ALL zustand stores
- * that are using devOnlyDevtools. This will allow you to use rewind state feature
+ * that are using such zustand middlewares:
+ * devOnlyDevtools(..)
+ * devtools(..)
+ * This will allow you to use rewind state feature
  * of redux devtools extension, without having to switch between different connection tabs from
  * the dropdown menu, which may be very inconvenient.
 */
